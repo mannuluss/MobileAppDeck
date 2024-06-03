f64744f3f1d8ac6a952aa343c3804006158cecdb
Felipe <pipe_050401@hotmail.com>
2024-06-03 14:53:53 -0500

    open deep link on android

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Component, NgZone, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
+import { AuthKeycloakService } from '@core/auth/services/AuthKeycloakLocal.service';
 import { LoaderService } from '@core/loader/services/loader.service';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -9,12 +14,57 @@ import { LoaderService } from '@core/loader/services/loader.service';
 export class AppComponent implements OnInit {
   isLoading: boolean = false;
 
-  constructor(private loaderService: LoaderService) {}
+  constructor(
+    private loaderService: LoaderService,
+    private platform: Platform,
+    private router: Router,
+    private zone: NgZone,
+    private authService: AuthKeycloakService
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.listenToAppUrlOpen();
+    });
+  }
+
+  /**
+   * Listen to the appUrlOpen event to handle deep links.
+   */
+  listenToAppUrlOpen() {
+    App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+      this.zone.run(() => {
+        const domain = 'mobile.feliperojas.live';
+        const path = event.url.split(domain).pop();
+
+        if (path) {
+          console.log('path', path);
+          this.router.navigateByUrl(path);
+        }
+      });
+    });
+  }
 
   ngOnInit(): void {
     this.loaderService.showLoader$.subscribe((value) => {
       console.log('spinner', value);
       this.isLoading = value;
     });
+
+    this.authService.init();
+    this.authService.isLogin().subscribe((isLogin) => {
+      if (isLogin) {
+        this.authService.profile().subscribe((data) => {
+          console.log('KEYCLOAK profile', data);
+          this.continue();
+        });
+      }
+    });
+  }
+
+  continue() {
+    this.router.navigate(['/card-games/votacion']);
   }
 }
